117dd38adf9f7a38d34632ae1d5c474d3c6af6c3
Mintscandy <3199608937@qq.com>
2025-01-01 15:58:40 +0800

    a

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,8 @@
 const serverConfig = {
-    baseURL: "http://localhost:8089", // 测试环境
-    // baseURL: "http://api.srcandy.top", // 生产环境
-    wsURL: "ws://localhost:8089/webssh", // WebSocket本地测试环境
-    // wsURL: "ws://sh.srcandy.top:8089/webssh", // WebSocket
+    // baseURL: "http://localhost:8089", // 测试环境
+    baseURL: "http://api.srcandy.top", // 生产环境
+    // wsURL: "ws://localhost:8089/webssh", // WebSocket本地测试环境
+    wsURL: "ws://sh.srcandy.top:8089/webssh", // WebSocket
     // wsLocalhostURL: "ws://localhost:8765/webssh", // WebSocket 本地测试环境
     useTokenAuthorization: true, // 是否开启 token 认证
 };
