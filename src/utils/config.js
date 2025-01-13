030b2c2508b48424a7611928e5a6c10598e810c8
Mintscandy <3199608937@qq.com>
2025-01-13 14:59:53 +0800

    新增了开发环境和测试环境的自动判断

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,13 @@
 const serverConfig = {
-    baseURL: "http://localhost:8089", // 测试环境
-    // baseURL: "http://api.srcandy.top", // 生产环境
-    wsURL: "ws://localhost:8089/webssh", // WebSocket本地测试环境
-    // wsURL: "ws://sh.srcandy.top:8089/webssh", // WebSocket
+    baseURL: process.env.NODE_ENV === 'production'
+        ? "http://api.srcandy.top" // 生产环境
+        : "http://localhost:8089", // 开发环境
+
+    wsURL: process.env.NODE_ENV === 'production'
+        ? "ws://sh.srcandy.top:8089/webssh" // 生产环境 WebSocket
+        : "ws://localhost:8089/webssh", // 开发环境 WebSocket
+
     useTokenAuthorization: true, // 是否开启 token 认证
 };
+
 export default serverConfig;
