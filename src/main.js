c716f10dea88210abaae62252fbc27cb24982ffe
Mintscandy <3199608937@qq.com>
2025-03-24 15:28:00 +0800

    remove:移除了一些没用的import

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 import naive from 'naive-ui/es/preset'
-// import './assets/index.css'
 
 
 createApp(App).use(naive).use(store).use(router).mount('#app')
