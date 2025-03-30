1b61ed3aeafebf0b8ba0bc9c892fba6fd5d94f22
Mintscandy <3199608937@qq.com>
2025-03-30 22:31:27 +0800

    凭证中心 ProfileView部分完成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-config-provider :theme="theme">
+  <n-config-provider :theme="theme" :hljs="hljs">
     <n-dialog-provider>
       <n-notification-provider>
         <n-message-provider>
@@ -13,6 +13,7 @@
 
 <script setup>
 import TerminalSettingsDrawer from '@/components/drawer/TerminalSettingsDrawer.vue';
+import hljs from 'highlight.js';
 // ... 其他代码保持不变
 </script>
 
