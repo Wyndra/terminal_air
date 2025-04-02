002cd2d91e7457f88f0aaf28e0d8595544259ace
Mintscandy <3199608937@qq.com>
2025-04-02 23:06:04 +0800

    启用HTTPS

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,9 @@
 const serverConfig = {
     baseURL: process.env.NODE_ENV === 'production'
-        ? "http://api.srcandy.top" // 生产环境
+        ? "https://api.srcandy.top" // 生产环境
         : "http://localhost:8089", // 开发环境
     wsURL: process.env.NODE_ENV === 'production'
-        ? "ws://sh.srcandy.top:8089/webssh" // 生产环境 WebSocket
+        ? "wss://sh.srcandy.top:8089/webssh" // 生产环境 WebSocket
         : "ws://localhost:8089/webssh", // 开发环境 WebSocket
     updateURL: 'http://static.srcandy.top/upload', // 更新地址
     useTokenAuthorization: true, // 是否开启 token 认证
